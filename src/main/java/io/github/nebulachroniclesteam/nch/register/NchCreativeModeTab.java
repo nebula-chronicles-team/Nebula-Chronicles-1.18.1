d0aec69cc992eb2edc8d1594b088c176900abc25
beanflame <beanflame@qq.com>
2022-02-05 11:08:42 +0800

    更新 Tab

--- src/main/java/io/github/nebulachroniclesteam/nch/register/NchCreativeModeTab.java
+++ src/main/java/io/github/nebulachroniclesteam/nch/register/NchCreativeModeTab.java
@@ -7,7 +7,7 @@ import net.minecraft.world.item.Items;
 
 public class NchCreativeModeTab {
 
-    public static final CreativeModeTab NCH_ITEMS = new CreativeModeTab(getLabel("nch_items")) {
+    public static final CreativeModeTab NCH_MISC_ITEMS = new CreativeModeTab(getLabel("nch_misc_items")) {
         public ItemStack makeIcon() {
             return new ItemStack(NchItems.WHITE_BUD_LEAVES.get());
         }
@@ -20,6 +20,12 @@ public class NchCreativeModeTab {
     };
 
     public static final CreativeModeTab NCH_WORLD = new CreativeModeTab(getLabel("nch_world")) {
+        public ItemStack makeIcon() {
+            return new ItemStack(Items.STONE);
+        }
+    };
+
+    public static final CreativeModeTab NCH_INDUSTRIAL_BLOCKS = new CreativeModeTab(getLabel("nch_industrial_blocks")) {
         public ItemStack makeIcon() {
             return new ItemStack(Items.STONE);
         }
--- src/main/java/io/github/nebulachroniclesteam/nch/register/NchItems.java
+++ src/main/java/io/github/nebulachroniclesteam/nch/register/NchItems.java
@@ -15,11 +15,11 @@ public class NchItems {
 
 
     public static final RegistryObject<Item>
-            // NCH_ITEMS
-            WHITE_BUD_LEAVES = ITEMS.register("white_bud_leaves", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_ITEMS))),
-            WHITE_BUD_STEW = ITEMS.register("white_bud_stew", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_ITEMS).food((new FoodProperties.Builder()).nutrition(6).saturationMod(0.3F).build()))),
-            LANTERN_BERRIES = ITEMS.register("lantern_berries", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_ITEMS).food((new FoodProperties.Builder()).nutrition(3).saturationMod(0.3F).effect(new MobEffectInstance(MobEffects.GLOWING, 300, 0), 1.0F).effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 300, 0), 1.0F).build()))),
-            BUG_FLESH = ITEMS.register("bug_flesh", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_ITEMS).food((new FoodProperties.Builder()).nutrition(2).saturationMod(0.3F).build()))),
+            // NCH_MISC_ITEMS
+            WHITE_BUD_LEAVES = ITEMS.register("white_bud_leaves", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_MISC_ITEMS))),
+            WHITE_BUD_STEW = ITEMS.register("white_bud_stew", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_MISC_ITEMS).food((new FoodProperties.Builder()).nutrition(6).saturationMod(0.3F).build()))),
+            LANTERN_BERRIES = ITEMS.register("lantern_berries", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_MISC_ITEMS).food((new FoodProperties.Builder()).nutrition(3).saturationMod(0.3F).effect(new MobEffectInstance(MobEffects.GLOWING, 300, 0), 1.0F).effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 300, 0), 1.0F).build()))),
+            BUG_FLESH = ITEMS.register("bug_flesh", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_MISC_ITEMS).food((new FoodProperties.Builder()).nutrition(2).saturationMod(0.3F).build()))),
 
     // NCH_INDUSTRIAL_ITEMS
     RAW_BLACK_TUNGSTEN = ITEMS.register("raw_black_tungsten", () -> new Item(new Item.Properties().tab(NchCreativeModeTab.NCH_INDUSTRIAL_ITEMS))),
